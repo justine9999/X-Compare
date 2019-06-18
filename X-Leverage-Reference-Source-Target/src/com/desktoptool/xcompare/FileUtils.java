985f1e1cd1d874dd19ff93e4228388cf655c7bb1
Minda Xiang <mxiang@transperfect.com>
2019-06-18 15:11:46 -0400

    remove unneccessary lib ref

--- X-Leverage-Reference-Source-Target/src/com/desktoptool/xcompare/FileUtils.java
+++ X-Leverage-Reference-Source-Target/src/com/desktoptool/xcompare/FileUtils.java
@@ -2,11 +2,9 @@ package com.desktoptool.xcompare;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
